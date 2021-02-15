3fb029120b9b6c749bf8eccecf0055325ded133b
matusstas <matusstas.ms@gmail.com>
2021-02-15 23:03:08 +0100

    gui updated, communication between plot and text area resolved

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from PyQt5.QtWidgets import QMainWindow, QDialog, QApplication
 
 from config import *
 
+import numpy as np
+
+from random import uniform
+
 class PlaygroundWindow(QDialog):
     
     def __init__(self):
@@ -16,6 +20,7 @@ class PlaygroundWindow(QDialog):
         # set up listeners
         self.pushButton.clicked.connect(self.goBack)
         self.pushButton_2.clicked.connect(self.getText)
+        self.pushButton_3.clicked.connect(self.generatePoints)
         self.horizontalSlider.valueChanged.connect(self.onValueChanged)
 
 
@@ -31,45 +36,72 @@ class PlaygroundWindow(QDialog):
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 
-    def onValueChanged(self):
-        pass
-
     def getText(self):
-        self.MatplotlibWidget.arr_drawing = []
+        self.MatplotlibWidget.animation._stop()
+
+        self.MatplotlibWidget.pen = False
+        self.MatplotlibWidget.arr_drawing = np.empty((1,2))
+
+        # clear and reload subplot
+        self.MatplotlibWidget.canvas.axis.clear()
+        self.MatplotlibWidget.canvas.axis.set_xlim(AXIS_LIMIT)
+        self.MatplotlibWidget.canvas.axis.set_ylim(AXIS_LIMIT)
+        self.MatplotlibWidget.line_draw.set_data([],[])
+        self.MatplotlibWidget.canvas.draw()
+
+        
         text = self.textEdit.toPlainText()
         text = text.split('\n')
 
-        if text[-1] == '':
-            text.pop()
+        # if text[-1] == '':
+        #     text.pop()
 
         arr_temp = []
         for coordinates in text:
             x,y = list(map(float, coordinates.strip().split(',')))
             arr_temp.append([x,y])
 
-        max_x = max(arr_temp, key=lambda x: x[0])[0]
-        max_y = max(arr_temp, key=lambda x: x[1])[1]
-        min_x = min(arr_temp, key=lambda x: x[0])[0]
-        min_y = min(arr_temp, key=lambda x: x[1])[1]
+        # max_x = max(arr_temp, key=lambda x: x[0])[0]
+        # max_y = max(arr_temp, key=lambda x: x[1])[1]
+        # min_x = min(arr_temp, key=lambda x: x[0])[0]
+        # min_y = min(arr_temp, key=lambda x: x[1])[1]
 
-        dx = (max_x-min_x)/2+min_x
-        dy = (max_y-min_y)/2+min_y
+        # dx = (max_x-min_x)/2+min_x
+        # dy = (max_y-min_y)/2+min_y
 
-        # update coordinates relative to center 0,0
-        arr_drawing = []
-        for coordinates in arr_temp:
-            x,y = coordinates
-            arr_drawing.append([x-dx,y-dy])
+        # # update coordinates relative to center 0,0
+        # arr_drawing = []
+        # for coordinates in arr_temp:
+        #     x,y = coordinates
+        #     arr_drawing.append([x-dx,y-dy])
 
-        # set axes borders
-        LIMIT = max((max_x-min_x)/2,(max_y-min_y)/2)
-        offset = LIMIT*0.5
-        self.MatplotlibWidget.canvas.axis.set_xlim([-LIMIT-offset,LIMIT+offset])
-        self.MatplotlibWidget.canvas.axis.set_ylim([-LIMIT-offset,LIMIT+offset])
+        # # set axes borders
+        # LIMIT = max((max_x-min_x)/2,(max_y-min_y)/2)
+        # offset = LIMIT*0.5
+        # self.MatplotlibWidget.canvas.axis.set_xlim([-LIMIT-offset,LIMIT+offset])
+        # self.MatplotlibWidget.canvas.axis.set_ylim([-LIMIT-offset,LIMIT+offset])
 
         # self.MatplotlibWidget.arr_drawing = arr_drawing
-        self.MatplotlibWidget.arr_drawing = arr_drawing
-        self.MatplotlibWidget.run()
+        self.MatplotlibWidget.arr_drawing = arr_temp
+        self.MatplotlibWidget.runAnimation()
+
+
+    def generatePoints(self):
+        ## clear all coordinates from text area
+        self.textEdit.clear()
+
+        i = self.horizontalSlider_2.value()
+        n = 2**(i+2)
+        for _ in range(n):
+            x = uniform(-20,20)
+            y = uniform(-20,20)
+            self.textEdit.append(f'{x}, {y}')
+
+
+    def onValueChanged(self):
+        n = self.horizontalSlider.value()
+        self.label_2.setText(str(n))
+
 
 class SecondWindow(QDialog):
     
--- matplotlibwidget.py
+++ matplotlibwidget.py
@@ -25,7 +25,7 @@ class MatplotlibWidget(QWidget):
 
         self.canvas = FigureCanvas(Figure(dpi=DPI))
         
-        # clip matplotlib canvas to layout
+        # attach matplotlib canvas to layout
         vertical_layout = QVBoxLayout()
         vertical_layout.addWidget(self.canvas)
         self.setLayout(vertical_layout)
@@ -41,17 +41,14 @@ class MatplotlibWidget(QWidget):
         # variables
         self.pressed = False
         self.plotted = False
-        self.arr_drawing_complex = []
-        self.arr_radius = []
+        self.pen = False
         self.N = 0
+        self.arr_drawing_complex = []
+        self.time = []
+        self.arr_coordinates = []
         self.line_draw, = self.canvas.axis.plot([], [], c='#eb4034', linewidth=1)
         self.arr_drawing = np.empty((1,2))
-        self.time = []
-        self.xxx2 = []
-        self.check = []
-        self.is_running = False
 
-        
         # listeners
         self.canvas.mpl_connect('button_press_event', self.on_press)
         self.canvas.mpl_connect('motion_notify_event', self.on_motion)
@@ -59,7 +56,7 @@ class MatplotlibWidget(QWidget):
 
 
     def on_press(self, event):
-        if self.arr_drawing[1:].size > 0:
+        if len(self.arr_drawing[1:]) > 0:
             self.animation._stop()
 
         if self.plotted:
@@ -75,14 +72,17 @@ class MatplotlibWidget(QWidget):
             self.canvas.draw()
 
         self.pressed = True
+        self.pen = True
+
 
 
     def on_motion(self, event):
         if self.pressed:
             self.plotted = True
 
-            # plot drawing points 
-            self.arr_drawing = np.append(self.arr_drawing, [[event.xdata, event.ydata]], axis=0)
+            # plot drawing points
+            coordinates = [event.xdata, event.ydata]
+            self.arr_drawing = np.append(self.arr_drawing, [coordinates], axis=0)
             self.line_draw.set_data(self.arr_drawing[1:,0],self.arr_drawing[1:,1])
             self.canvas.axis.draw_artist(self.line_draw)
             self.canvas.blit(self.canvas.axis.bbox)
@@ -91,11 +91,10 @@ class MatplotlibWidget(QWidget):
 
     def on_release(self, event):
         self.pressed = False
-        self.line_draw.set_xdata([])
-        self.line_draw.set_ydata([])
+        self.line_draw.set_data([],[])
 
         if len(self.arr_drawing) > 0:
-            self.run()
+            self.runAnimation()
 
 
     def getSizes(self):
@@ -106,36 +105,39 @@ class MatplotlibWidget(QWidget):
         return size_pt
 
 
-    def run(self):
-        self.is_running = True
+    def runAnimation(self):
         self.animation = animation.FuncAnimation(
                             self.canvas.figure,
                             self.animate,
                             init_func=self.init,
-                            interval=25,
+                            interval=ANIMATION_INTERVAL,
                             blit=True)
 
 
     def init(self):
-        self.arr_drawing_complex = [complex(coordinates[0], coordinates[1]) for coordinates in self.arr_drawing[1:]]
+        # set new values
+        if self.pen:
+            self.arr_drawing_complex = [complex(coordinates[0], coordinates[1]) for coordinates in self.arr_drawing[1:]]
+        else:
+            self.arr_drawing_complex = [complex(coordinates[0], coordinates[1]) for coordinates in self.arr_drawing[:]]
+        
         self.N = len(self.arr_drawing_complex)
         self.CanvasWindow.horizontalSlider.setMaximum(self.N)
         self.CanvasWindow.horizontalSlider.setValue(self.N)
 
+        # calculate fourier transform via FFT algorithm
         self.ft = FourierTransform(self.arr_drawing_complex)
         self.ft.toEpicycles()
 
-        self.time = np.linspace(0,2*pi,endpoint = False, num=self.N)        
+        # calculate all points in time range from 0 to 2pi
+        self.time = np.linspace(0,2*pi,endpoint = False, num=self.N)
+        self.arr_coordinates = np.array([self.ft.getPoint(dt) for dt in self.time])
 
-        self.xxx2 = np.array([self.ft.getPoint(dt) for dt in self.time])
-
+        # create all components responsible for showing animation
         self.circle = self.canvas.axis.scatter([],[], fc='None', ec='#9ac7e4', lw=1)
         self.circle.set_sizes(self.getSizes())
-
         self.line_connect, = self.canvas.axis.plot([], [], c='#9ac7e4', lw=1)
-
         self.line_plot, = self.canvas.axis.plot([], [], c='#4c6bd5', lw=2)
-        
         self.line_plot_all, = self.canvas.axis.plot([], [], c='#4c6bd5', lw=0.5)
 
         return [self.circle, self.line_connect, self.line_plot, self.line_plot_all]
@@ -144,12 +146,10 @@ class MatplotlibWidget(QWidget):
     def animate(self,i):
         s = self.CanvasWindow.horizontalSlider.value()+1
 
-        self.circle.set_offsets(self.xxx2[:,:s][i%self.N,:-1])
-
-        self.line_connect.set_data(self.xxx2[:,:s][i%self.N,:,0],self.xxx2[:,:s][i%self.N,:,1])
-
-        self.line_plot.set_data(self.xxx2[:,:s][:i%self.N,-1,0],self.xxx2[:,:s][:i%self.N,-1,1])
-        
-        self.line_plot_all.set_data(self.xxx2[:,:s][:,-1,0],self.xxx2[:,:s][:,-1,1])
+        # update components
+        self.circle.set_offsets(self.arr_coordinates[:,:s][i%self.N,:-1])
+        self.line_connect.set_data(self.arr_coordinates[:,:s][i%self.N,:,0],self.arr_coordinates[:,:s][i%self.N,:,1])
+        self.line_plot.set_data(self.arr_coordinates[:,:s][:i%self.N+1,-1,0],self.arr_coordinates[:,:s][:i%self.N+1,-1,1])
+        self.line_plot_all.set_data(self.arr_coordinates[:,:s][:,-1,0],self.arr_coordinates[:,:s][:,-1,1])
 
         return [self.circle, self.line_connect, self.line_plot, self.line_plot_all]
